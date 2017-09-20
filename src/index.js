74e88a20ad6736eae4f149c8308ff20ca68ef133
Natalie Villasana <navillasa@gmail.com>
2017-09-20 12:57:39 -0400

    annotates first steps in creating an element and putting it on the dom to render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// Go find the library called react in my dependencies, and
+// assign it to the variable called 'react'
+import React from 'react';
+// The core React library knows how to work with React components.
+// but the module to render them is in React-DOM.
+// To render to the DOM, we don't use React, we use React-dom.
+import ReactDOM from 'react-dom';
+
 // What is React?
 
 // React is a JS library used to produce HTML shown to a user in a browser.
@@ -6,3 +14,13 @@
 
 // We nest these components by placing one inside the other to make complex applications more simple.
 // When we write in React, we're writing JS but it will ultimately produce HTML.
+
+// 1.
+// Create a new component that will produce some HTML
+const App = () => {
+    return <div>myellow</div>;
+}
+
+// 2.
+// Take this component's generated HTML and put it in the DOM
+ReactDOM.render(App);
