be46f26620ab8406dc3cd1af7146e0047e6998ae
Natalie Villasana <navillasa@gmail.com>
2017-09-20 12:31:14 -0400

    removes src boiler plate, creates new empty src folder with notes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// What is React?
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// React is a JS library used to produce HTML shown to a user in a browser.
+// When we write React code, we're writing individual React components, or views.
+// Components are individuals snippets of code that PRODUCE HTML.
+
+// We nest these components by placing one inside the other to make complex applications more simple.
+// When we write in React, we're writing JS but it will ultimately produce HTML.
