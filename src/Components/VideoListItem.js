eb8e2b69d14f8e8683ba79e16f31391de6457b29
Natalie Villasana <navillasa@gmail.com>
2017-09-25 13:14:56 -0400

    adds event handler onInputChange as a prop (function) attached to input

--- src/Components/VideoListItem.js
+++ src/Components/VideoListItem.js
@@ -11,11 +11,31 @@ import React, { Component } from 'react';
 // Steph recommends starting off with a functional component
 // Only when you need added functionality, then refactor with a class
 
+// Now when the user types something, we want to know that they're typing something
+// We need an eventHandler, where we pass the element we want to monitor for events
+
+// things are a little jank right now, because at first
+// I didn't want to do a search bar, but now I do...
+
 class VideoListItem extends Component {
     // Every class must have a render method
     // That returns JSX in the render method
     render() {
-        return <input />;
+        // All HTML input elements emit a change event
+        // to tap into a vanilla browser even like that
+        // we just type on""={this.eventHanderName}
+        // Whenever we use javascript variables in JSX--
+        //      we wrap the variables in curly braces
+        
+        // Creates a new imput element, and passes a PROP
+        // onChange with a value of {this.onInputChange}
+        return <input onChange={this.onInputChange} />;
+    }
+
+    // whenever we add an eventHandler, it needs to
+    // be passed an event object
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 }
 
